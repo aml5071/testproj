fd0f1d0279be163495363c16b8df731671c44856
Adam Laubham <46584739+aml5071@users.noreply.github.com>
2019-03-03 18:30:02 -0500

    fixed travis

--- my-app/src/karma.conf.js
+++ my-app/src/karma.conf.js
@@ -25,10 +25,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false,
     customLaunchers: {
-      ChromeHeadless: {
+      ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }

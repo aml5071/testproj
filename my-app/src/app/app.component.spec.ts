4ae550ae65f72393819bfc529384a295e66ec896
Adam Laubham <46584739+aml5071@users.noreply.github.com>
2019-03-03 18:54:06 -0500

    made test pass again

--- my-app/src/app/app.component.spec.ts
+++ my-app/src/app/app.component.spec.ts
@@ -27,6 +27,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
-    expect(true).toBe(false);
   });
 });

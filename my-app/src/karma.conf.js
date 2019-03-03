9d8f420083e7846a75b75a193cfd47ad3ed2c109
Adam Laubham <46584739+aml5071@users.noreply.github.com>
2019-03-03 16:29:42 -0500

    Modified karma config

--- my-app/src/karma.conf.js
+++ my-app/src/karma.conf.js
@@ -26,6 +26,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
   });
 };
+
+
